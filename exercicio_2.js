d86f1326c9d675a902156b8a2c068ee8926aff7b
Felipe Miller <fmvargas@protonmail.com>
2023-08-24 21:51:26 -0300

    exercício dois

--- exercicio_2.js
+++ exercicio_2.js
@@ -1,1 +1,11 @@
-        
+const notebook = {
+    Processador: "i7",
+    Memória: "16GB",
+    Preço: 5000,
+    HD: "1TB",
+    SSD: "256GB"
+};
+
+for (const atributo in notebook) {
+    console.log(atributo + ": " + notebook[atributo]);
+}        
