137641206a99bddb7c2bac5089462751cbe115af
Felipe Miller <fmvargas@protonmail.com>
2023-08-24 21:50:11 -0300

    exercício um

--- exercicio_1.js
+++ exercicio_1.js
@@ -1,1 +1,7 @@
-        
+const produto = {
+    preco: 90,
+    descricao: "Mouse"
+};
+
+console.log("Preço do produto:", produto.preco);
+console.log("Descrição do produto:", produto.descricao);  
